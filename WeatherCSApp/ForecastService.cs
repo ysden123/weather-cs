37333094b7937f908d14177aa37e914bb9a5c36b
Yuriy Stul <ysden123@gmail.com>
2025-09-21 14:24:08 +0300

    Icons from resources from DLL

--- WeatherCSApp/ForecastService.cs
+++ WeatherCSApp/ForecastService.cs
@@ -1,22 +1,23 @@
-﻿using WeatherCSLib.Data;
+﻿using WeatherCSLib;
+using WeatherCSLib.Data;
 
-namespace WeatherCSLib
+namespace WeatherCSApp
 {
     public class ForecastService
     {
         private static readonly Dictionary<string, string> icons = new()
         {
-            {"clear", "Resources\\clear.png" },
-            {"clear-day", "Resources\\clear-day.png" },
-            {"clear-night", "Resources\\clear-night.png" },
-            {"cloudy", "Resources\\cloudy.png" },
-            {"fog", "Resources\\fog.png" },
-            {"partly-cloudy-day", "Resources\\partly-cloudy-day.png" },
-            {"partly-cloudy-night", "Resources\\partly-cloudy-night.png" },
-            {"rain", "Resources\\rain.png" },
-            {"sleet", "Resources\\sleet.png" },
-            {"snow", "Resources\\snow.png" },
-            {"wind", "Resources\\wind.png" },
+            {"clear", "/Resources/clear.png" },
+            {"clear-day", "/Resources/clear-day.png" },
+            {"clear-night", "/Resources/clear-night.png" },
+            {"cloudy", "/Resources/cloudy.png" },
+            {"fog", "/Resources/fog.png" },
+            {"partly-cloudy-day", "/Resources/partly-cloudy-day.png" },
+            {"partly-cloudy-night", "/Resources/partly-cloudy-night.png" },
+            {"rain", "/Resources/rain.png" },
+            {"sleet", "/Resources/sleet.png" },
+            {"snow", "/Resources/snow.png" },
+            {"wind", "/Resources/wind.png" },
         };
 
         public static async Task<Forecast> GetForecast(City city)
@@ -82,16 +83,7 @@ namespace WeatherCSLib
                 return null;
 
             icons.TryGetValue(iconName, out string? iconPath);
-            string? theIconPath;
-            if (iconPath != null)
-            {
-                theIconPath = Path.Combine(Directory.GetCurrentDirectory(), iconPath);
-            }
-            else
-            {
-                theIconPath = null;
-            }
-            return theIconPath;
+            return iconPath;
         }
     }
 }
